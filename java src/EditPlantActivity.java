603777f0d192937a945525ccc5cac8b670df4414
himty <jenny.wang244@gmail.com>
2017-06-15 12:25:30 -0700

    Add edit plant activity (empty)
    More things to do

---
+++ java src/EditPlantActivity.java
@@ -0,0 +1,17 @@
+package com.plant.plant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_plant);
+    }
+
+    public void savePlant() {
+        //TODO: save plant
+    }
+}
