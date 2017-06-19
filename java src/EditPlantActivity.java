e3b66e41ba2c25a561dba8afb85f2c0f9c2fbf65
himty <jenny.wang244@gmail.com>
2017-06-19 14:49:18 -0700

    Delete watering interval seekbar
    rip :c

--- java src/EditPlantActivity.java
+++ java src/EditPlantActivity.java
@@ -2,17 +2,19 @@ package com.plant.plant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class EditPlantActivity extends AppCompatActivity {
     final String TAG = "EditPlantActivity";
 
-    private static TextView wateringIntervalText;
+    private static EditText wateringIntervalDays;
     private static SeekBar wateringIntervalSeek;
     private static ImageView plantImage;
     private static TextView notesText;
@@ -21,42 +23,11 @@ public class EditPlantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_plant);
-        initWateringInterval();
         initPlantImage();
-        initNotesText();
     }
 
     public void savePlant() {
         //TODO: save plant in memory
-    }
-
-    public void initWateringInterval() {
-        wateringIntervalText = (TextView) findViewById(R.id.wateringIntervalDays);
-        wateringIntervalSeek = (SeekBar) findViewById(R.id.wateringIntervalSeek);
-        wateringIntervalText.setText(wateringIntervalSeek.getProgress() + " Days");
-
-        final int MAX = 20;
-        final int MIN = 1;
-        wateringIntervalSeek.setMax(MAX - MIN);
-        wateringIntervalText.setText(MIN + " Days");
-
-        wateringIntervalSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int value = wateringIntervalSeek.getProgress() + MIN;
-                wateringIntervalText.setText(value + " Days");
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
 
     public void initPlantImage() {
@@ -65,14 +36,11 @@ public class EditPlantActivity extends AppCompatActivity {
         plantImage.setImageResource(R.drawable.no_picture);
     }
 
-    public void initNotesText() {
-//        ScrollView scroller = new ScrollView(this);
-        notesText = (TextView) findViewById(R.id.notesText);
-//        scroller.addView(notesText);
-        notesText.setMovementMethod(new ScrollingMovementMethod());
-    }
-
     public void setLocation() {
         //TODO: record location of phone
+    }
+
+    public void takePicture() {
+        //TODO: start camera app
     }
 }
